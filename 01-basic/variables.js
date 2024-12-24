867dd90f21d620ccdabb44e00cf8a11f4dd54802
unnatitiwari <122891927+unnatitiwari@users.noreply.github.com>
2024-12-24 11:43:53 +0000

    variables/conversion

---
+++ 01-basic/variables.js
@@ -0,0 +1,50 @@
+const accountid=12345;
+
+
+let accountholder='sujeet'
+
+var accountpassword='1245687'
+
+accountcity='maihar'
+let aaccountstate;
+
+
+// accountid = 98682;  //const will not change after decleartation;
+
+console.log(accountid)
+
+// type of variables
+console.log(typeof accountid)
+
+// datatype conversion
+
+let score='223as'
+console.log(score)
+
+console.log( typeof score)
+let scorevalueinNum=Number(score)
+console.log(typeof scorevalueinNum)  //its change into a numberform 
+
+console.log( scorevalueinNum)//  NaN(not a number) '223as ' was changed in number from but ectual value of scorevalueinNum is nan
+
+
+let isloggedin='hitesh'
+
+let isloggedinBoolean= Boolean(isloggedin)
+console.log(isloggedinBoolean) //true   but when the value of isloggedin is empty'' then isloggedinboalean value is false
+
+// 1=true; 0= false
+//""=false
+// "hitesh"=true
+
+let highscore= 33
+ 
+console.log( typeof highscore)
+let stringhighscore= String(highscore)
+console.log( typeof stringhighscore)  //string
+console.log( stringhighscore) 
+
+
+// ********************operation
+
+
